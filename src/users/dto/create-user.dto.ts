f9aeac9d80581b79ef791d32ca535cf9528191f5
Cesar <cardona.cesar@outlook.es>
2023-08-25 14:19:00 -0600

    feat: Change user crete DTO

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,29 @@
-export class CreateUserDto {}
+import { IsDate, IsOptional, IsString, MinLength } from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  @MinLength(2)
+  firstname: string;
+
+  @IsString()
+  @MinLength(4)
+  lastname: string;
+
+  @IsString()
+  email: string;
+
+  @IsString()
+  @MinLength(10)
+  phone: string;
+
+  @IsString()
+  birthday: string;
+
+  @IsDate()
+  @IsOptional()
+  createdAt: Date;
+
+  @IsDate()
+  @IsOptional()
+  updatedAt: Date;
+}
