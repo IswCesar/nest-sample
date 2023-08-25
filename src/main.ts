a31c985d6946c472cfa87f53f1bd437eb42b368b
Cesar <cardona.cesar@outlook.es>
2023-08-25 14:40:19 -0600

    feat: Add swagger config

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Global prefix for API
   app.setGlobalPrefix('api');
+
+  // Global PIPES for input in API
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -12,6 +17,18 @@ async function bootstrap() {
     }),
   );
 
-  await app.listen(3000);
+  // Swagger + OpenAPI config
+  const config = new DocumentBuilder()
+    .setTitle('INSSOFT API')
+    .setDescription('TEST DB')
+    .setVersion('1.0')
+    .build();
+
+  // Use Swagger
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  // await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
