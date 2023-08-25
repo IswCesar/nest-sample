452f377f00224c03ea97c89f6ba89252baa2fa99
Cesar <cardona.cesar@outlook.es>
2023-08-25 14:15:41 -0600

    feat: Add user resouce

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
@@ -16,6 +17,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       // Just for dev purpose
       synchronize: true,
     }),
+    UsersModule,
   ],
   controllers: [],
   providers: [],
