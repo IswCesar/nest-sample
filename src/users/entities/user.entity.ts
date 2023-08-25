243dfa1900c11387210a592e7afcb072531410e0
Cesar <cardona.cesar@outlook.es>
2023-08-25 14:18:23 -0600

    feat: Change user entity

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,34 @@
-export class User {}
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt: Date;
+
+  @Column({ type: 'text', default: 'One firstname' })
+  firstname: string;
+
+  @Column({ type: 'text', default: 'One lastname' })
+  lastname: string;
+
+  @Column({ type: 'text', default: 'One email' })
+  email: string;
+
+  @Column({ type: 'text', default: 'One phone' })
+  phone: string;
+
+  @Column({ type: 'date', default: new Date() })
+  birthday: string;
+}
